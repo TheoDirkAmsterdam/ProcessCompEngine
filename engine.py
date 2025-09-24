0197533fe2fd8070458fcc1d9d6a671f08f2aa2b
Theo Dirk Meijler <tdmeijler@t-online.de>
2025-09-24 18:22:16 +0200

    Added classes to engine.py

--- engine.py
+++ engine.py
@@ -2,27 +2,45 @@ from simplexclient.objectclient import SimplexClass
 
 import random
 
-class Activity(SimplexClass):
+class Job(SimplexClass):
   connection='default'
   classTitle='Activity'
 
-  def createContainer(self):
-    newContainer=ChildrenContainer(ndx=str(random.random()))
+  def createRelationshipBindingContainer(self):
+    newContainer=RelationshipBindingContainer(ndx=str(random.random()))
     newContainer.save()
-    self.link('parent2container',newContainer)
+    self.link('job2relationshipcontainer',newContainer)
     return newContainer
 
 class Port(SimplexClass):
   connection='default'
   classTitle='Port'
 
-class ChildrenContainer(SimplexClass):
+class RelationshipBindingContainer(SimplexClass):
   connection='default'
-  classTitle='ChildrenContainer'
+  classTitle='RelationshipBindingContainer'
 
-class ChildBinding(SimplexClass):
+class RelationshipBinding(SimplexClass):
   connection='default'
-  classTitle='ChildBinding'
+  classTitle='RelationshipBinding'
+
+class InputPort(SimplexClass):
+  connection='default'
+  classTitle='InputPort'
+
+class OutputPort(SimplexClass):
+  connection='default'
+  classTitle='OutputPort'
+
+class PortCoupling(SimplexClass):
+  connection='default'
+  classTitle='PortCoupling'
+
+class SequenceController(SimplexClass):
+  connection='default'
+  classTitle='SequenceController'
+
+
 
 
 for cls in [Activity,Port,ChildrenContainer,ChildBinding]:
